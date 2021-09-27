d5c81d41bd7aaa7df401a73cd6fba71af0c2922b
liangtianyu <liangtianyu@uniondrug.cn>
2021-09-27 14:51:29 +0800

    Change SyncResult field SyncParam type to inteface{}

--- sync_result.go
+++ sync_result.go
@@ -1,12 +1,12 @@
 package golib
 
 type SyncResult struct {
-	SyncParam  SyncParam     `json:"syncParam"`
+	SyncParam  interface{}   `json:"syncParam"`
 	Table      string        `json:"table"`
 	TotalCount int64         `json:"totalCount"`
 	Result     []interface{} `json:"result"`
 }
 
-func NewSyncResult(syncParam SyncParam) *SyncResult {
+func NewSyncResult(syncParam interface{}) *SyncResult {
 	return &SyncResult{SyncParam: syncParam, Result: make([]interface{}, 0)}
 }
